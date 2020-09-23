e94aae7d31dffd11b01a05a0d5ba3128765f3246
tuanpth1909 <60849085+tuanpth1909@users.noreply.github.com>
2020-09-23 11:50:31 +0700

    Add project files.

---
+++ Views/QuanLiSinhVien.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage/HomeMaster.Master" AutoEventWireup="true" CodeBehind="QuanLiSinhVien.aspx.cs" Inherits="QLSV_Dialog.Views.QLSV" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="../css/myStyle.css" rel="stylesheet" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div id="form-dialog" class="bg-dark">
+        <form action="/" method="post">
+            <fieldset>
+                <h3 class="text-center">Add student profile</h3>
+                <hr class="bg-white" />
+                <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" name="name" id="txtName" class="w-75 float-right" value="" />
+                </div>
+                <div class="form-group">
+                    <label>Birth Day:</label>
+                    <input type="date" name="date" id="datBirthDay" class="w-75 float-right" value="" />
+                </div>
+                <div class="form-group">
+                    <label>Sex:</label><br />
+                    <input type="radio" name="sex" value="Male" checked />
+                    Male
+                    <br />
+                    <input type="radio" name="sex" value="Female" />
+                    Female
+                    <br />
+                </div>
+                <div class="form-group">
+                    <label>Address:</label>
+                    <input type="text" name="add" id="txtAddress" class="w-75 float-right" value="" />
+                </div>
+                <div class="form-group">
+                    <label>Tel:</label>
+                    <input type="number" name="tel" id="numTel" class="w-75 float-right" value="" />
+                </div>
+                <div class="form-group">
+                    <label>Email:</label>
+                    <input type="email" name="email" id="emlEmail" class="w-75 float-right" value="" />
+                </div>
+                <button type="button" name="btnSave" id="btnSave" class="btn btn-primary w-25 text-center">Save</button>
+                <button type="button" name="btnUpdate" id="btnUpdate" class="btn btn-success w-25 text-center" style="display: none">Update</button>
+                <button type="button" name="btnSave" id="btnCancel" class="btn btn-secondary w-25 text-center">Cancel</button>
+            </fieldset>
+        </form>
+    </div>
+    <div class="container mt-5">
+        <div>
+            <h2 class="text-center bg-info text-light p-2 rounded">LIST OF STUDENTS</h2>
+            <div class="mt-4 mb-3 row">
+                <div class="col-4">
+                    <button type="button" name="open" id="openDialog" class="btn btn-primary">Add</button>
+                </div>
+                <div class="col-8">
+                    <input type="search" name="name" id="search" class="float-right form-control w-50" value="" placeholder="Search..." />
+                </div>
+                
+            </div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Sex</th>
+                        <th>Address</th>
+                        <th>Tel</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Code</td>
+                        <td>2020-09-22</td>
+                        <td>Male</td>
+                        <td>Ha Noi</td>
+                        <td>0982513015</td>
+                        <td>code@gmail.com</td>
+                        <td>
+                            <button type="button" name="btnEdit" class="btn btn-warning ">Edit</button>
+                            <button type="button" name="btnDelete" class="btn btn-danger">Delete</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</asp:Content>
---
+++ js/jsQLSV.js
@@ -0,0 +1,182 @@
+﻿$(document).ready(function () {
+    $('#form-dialog').dialog({
+        autoOpen: false,
+        height: 550,
+        width: 500,
+        modal: true,
+        show: {
+            effect: 'scale',
+            duration: 250,
+        },
+        hide: {
+            effect: 'drop',
+            duration: 200,
+        }
+    });
+
+    $('#openDialog').click(function () {
+        $('#form-dialog').dialog('open');
+    });
+
+    //Them moi sinh vien
+    addStudents();
+
+    //Xoa sinh vien
+    deleteStudents();
+
+    //Tim kiem sinh vien
+    searchStudents();
+
+    //Chinh sua sinh vien
+    editStudents();
+
+    //thoat
+    cancelStudents();
+});
+
+//Hàm thêm mới hồ sơ sinh vien
+function addStudents() {
+    $('#btnSave').click(function () {
+        //lay gia tri tu cac input
+        let name = $('#txtName').val();
+        let dob = $('#datBirthDay').val();
+        let sex = $('input[name=sex]:checked').val();
+        let add = $('#txtAddress').val();
+        let tel = $('#numTel').val();
+        let email = $('#emlEmail').val();
+
+        //chen du lieu vao table
+        $('tbody').prepend("<tr>" +
+            "<td>" + name + "</td>" +
+            "<td>" + dob + "</td>" +
+            "<td>" + sex + "</td>" +
+            "<td>" + add + "</td>" +
+            "<td>" + tel + "</td>" +
+            "<td>" + email + "</td>" +
+            "<td><button type='button' name='btnEdit' class='btn btn-warning mr-1'>Edit</button><button type='button' name='btnDelete' class='btn btn-danger'>Delete</button></td>" +
+            "</tr>");
+
+        $('#txtName').val("");
+        $('#datBirthDay').val("");
+        $('#txtAddress').val("");
+        $('#numTel').val("");
+        $('#emlEmail').val("");
+
+        $('#form-dialog').dialog('close');
+
+        alert("Added successfully!");
+
+        //xoa ban ghi
+        deleteStudents();
+
+        //tim kiem ban ghi
+        searchStudents();
+
+        //sua ban ghi
+        editStudents();
+
+        //thoat
+        cancelStudents();
+
+    });
+}
+
+//Hàm xóa hồ sơ sinh viên
+function deleteStudents() {
+    $('button[name=btnDelete]').click(function () {
+        $(this).closest('tr').remove();
+    });
+}
+
+//Hàm tìm kiếm hồ sơ 
+function searchStudents() {
+    $('#search').on('keyup', function () {
+        let keyword = $(this).val().toLowerCase();
+        $('tbody tr').filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(keyword) > -1);
+        });
+    });
+}
+
+//Hàm chỉnh sửa thông tin sinh viên
+function editStudents() {
+
+    let trEdit;
+
+    //bat su kien khi nhap chuot vao Edit
+    $('button[name=btnEdit]').click(function () {
+
+        $('#form-dialog').dialog('open');
+
+        trEdit = $(this).closest('tr');
+        $('#btnSave').hide();
+        $('#btnUpdate').show();
+
+        let name = $(trEdit).find('td:eq(0)').text();
+        let dob = $(trEdit).find('td:eq(1)').text();
+        let sex = $(trEdit).find('td:eq(2)').text();
+        let add = $(trEdit).find('td:eq(3)').text();
+        let tel = $(trEdit).find('td:eq(4)').text();
+        let email = $(trEdit).find('td:eq(5)').text();
+
+        $('#txtName').val(name);
+        $('#datBirthDay').val(dob);
+        //$('input[name=sex]:checked').val(sex);
+        $('input[name=sex][value=' + sex + ']').prop("checked", true);
+        $('#txtAddress').val(add);
+        $('#numTel').val(tel);
+        $('#emlEmail').val(email);
+
+
+    });
+
+    //bat su kiem khi nhap chuot vao update
+    $('#btnUpdate').click(function () {
+        if (trEdit) {
+            //lay du lieu da duoc chinh sua
+            let name = $('#txtName').val();
+            let dob = $('#datBirthDay').val();
+            let sex = $('input[name=sex]:checked').val();
+            let add = $('#txtAddress').val();
+            let tel = $('#numTel').val();
+            let email = $('#emlEmail').val();
+
+            //thay the du lieu cu sau khi chinh sua
+            $(trEdit).find('td:eq(0)').text(name);
+            $(trEdit).find('td:eq(1)').text(dob);
+            $(trEdit).find('td:eq(2)').text(sex);
+            $(trEdit).find('td:eq(3)').text(add);
+            $(trEdit).find('td:eq(4)').text(tel);
+            $(trEdit).find('td:eq(5)').text(email);
+
+            trEdit = null;
+
+            $('#txtName').val("");
+            $('#datBirthDay').val("");
+            $('#txtAddress').val("");
+            $('#numTel').val("");
+            $('#emlEmail').val("");
+
+            $('#btnSave').show();
+            $('#btnUpdate').hide();
+
+            //$('form-dialog').dialog('close');
+            $('#form-dialog').dialog('close');
+
+        }
+
+    });
+
+}
+
+//Hàm thoát
+function cancelStudents() {
+    $('#btnCancel').click(function () {
+        $('#txtName').val("");
+        $('#datBirthDay').val("");
+        $('#txtAddress').val("");
+        $('#numTel').val("");
+        $('#emlEmail').val("");
+        $('#form-dialog').dialog('close');
+    });
+}
